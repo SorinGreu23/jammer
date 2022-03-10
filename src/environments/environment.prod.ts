89ade0c22ce3e7529354a4b69e340ef49e633f59
Sorin Greu <greusorin2003@gmail.com>
2022-03-10 17:48:27 +0200

    Added Firebase to the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB6OpHSnFcoNu14Z3S5E0TF4G9gypPbzZ4',
+    authDomain: 'jammer-e6a63.firebaseapp.com',
+    projectId: 'jammer-e6a63',
+    storageBucket: 'jammer-e6a63.appspot.com',
+    messagingSenderId: '828586405039',
+    appId: '1:828586405039:web:9293104982805e8e55385a',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB6OpHSnFcoNu14Z3S5E0TF4G9gypPbzZ4',
+    authDomain: 'jammer-e6a63.firebaseapp.com',
+    projectId: 'jammer-e6a63',
+    storageBucket: 'jammer-e6a63.appspot.com',
+    messagingSenderId: '828586405039',
+    appId: '1:828586405039:web:9293104982805e8e55385a',
+  },
 };
 
 /*
