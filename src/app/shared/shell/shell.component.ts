d0218142996eee160017f8874f6311e6c2df1806
Sorin Greu <greusorin2003@gmail.com>
2022-03-10 17:01:08 +0200

    Initial Commit: App UI Implementation

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,24 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent implements OnInit {
+  //checking the type of device in order to properly display the menu
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+  }
+
+}
